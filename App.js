f4c7b1e24e3d5bf6cfbebe926c2930e2be4c8618
usssreddy <umasr25@gmail.com>
2026-04-15 11:36:40 +0530

    third epsiode

--- App.js
+++ App.js
@@ -14,11 +14,55 @@ import ReactDOM from "react-dom/client";
  */
 
 
-  const heading = React.createElement("div", {id : "parent"}, 
-               React.createElement("div", {id :"child"} , 
-               React.createElement("h1" , {} , "i am h1 tag")
-             )
-            );
+//   const heading = React.createElement("div", {id : "parent"}, 
+//                React.createElement("div", {id :"child"} , 
+//                React.createElement("h1" , {} , "i am h1 tag")
+//              )
+//             );
+
+
+const JsxHeading = () => ( 
+  <h1 className = "heading" style={{backgroundColor :"green",}}>  this  syntd form react </h1>
+);
+
+
+// React Functional component 
+// commponet compostion
+const HedingFunction = () => (
+        
+        <div>
+            <JsxHeading/>
+             <h1>this functional component</h1>;
+        </div>
+       
+);
+
+const Header = () => (
+    <div
+  id="container"
+  style={{
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "10px",
+  }}
+>
+  <img src="/ANL07074.jpg" alt="left-image" width="100" />
+
+  <input
+    type="search"
+    placeholder="Search..."
+    style={{
+      width: "300px",
+      padding: "10px",
+      borderRadius: "5px",
+    }}
+  />
+
+  <img src="/ANL07074.jpg" alt="right-image" width="100" />
+</div>
+);
+
 
 
 
@@ -27,4 +71,4 @@ import ReactDOM from "react-dom/client";
 
    const root =  ReactDOM.createRoot(document.getElementById("root"));
 
-   root.render(heading);
+   root.render(<Header/>);
